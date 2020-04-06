f454287a889462062f1753d103cc6d3ca559d901
Julian Tallar <jtallar@itba.edu.ar>
2020-04-06 11:12:49 -0300

    Added empty schema.sql, changed bd credentials, changed web.xml header
    Created empty schema.sql and added resource in WebConfig
    Changed BD credentials to root root 
    Deleted web.xml DOCTYPE, now xml tag

--- paw/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ paw/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,9 +1,14 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -15,6 +20,9 @@ import javax.sql.DataSource;
 @ComponentScan({ "ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.service", "ar.edu.itba.paw.persistence" })
 @Configuration
 public class WebConfig {
+    @Value("classpath:schema.sql")
+    private Resource schemaSql;
+
     @Bean
     public ViewResolver viewResolver() {
         final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -30,8 +38,21 @@ public class WebConfig {
         final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(org.postgresql.Driver.class);
         dataSource.setUrl("jdbc:postgresql://localhost/paw");
-        dataSource.setUsername("mati"); // TODO usuario linux
-        dataSource.setPassword("ojo_esta_es_mi_password"); // TODO tu contrasena
+        dataSource.setUsername("root"); // TODO usuario linux
+        dataSource.setPassword("root"); // TODO tu contrasena
         return dataSource;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
+        final DataSourceInitializer dsi = new DataSourceInitializer();
+        dsi.setDataSource(ds);
+        dsi.setDatabasePopulator(databasePopulator());
+        return dsi;
+    }
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
+//        dbp.addScript(schemaSql); // TODO Ponerle el create a schema.sql sino tira error por estar vacio
+        return dbp;
     }
 }
