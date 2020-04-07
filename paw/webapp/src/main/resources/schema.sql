ec6b837e05631bf959fa7eb8ca276bfabce9b3b7
mbrula <mbrula@itba.edu.ar>
2020-04-06 22:21:30 -0300

    Add project create table & fix errors

--- paw/webapp/src/main/resources/schema.sql
+++ paw/webapp/src/main/resources/schema.sql
@@ -3,28 +3,27 @@
 ** Each references the previous one.
 ** To avoid user manual input.
 */
-CREATE TABLE countries (
-    -- Country ID.
+CREATE TABLE IF NOT EXISTS countries (
     id          SERIAL PRIMARY KEY,
-    -- Country name and phone prefix.
     country     VARCHAR(25),
+
+    -- Prefix of the country and length of the number after the prefix.
     prefix      SMALLINT,
+    num_length  SMALLINT
 );
 
-CREATE TABLE provinces (
-    -- Province ID and respective country ID.
+CREATE TABLE IF NOT EXISTS provinces (
     id          SERIAL PRIMARY KEY,
     country_id  INT REFERENCES countries ON DELETE CASCADE,
-    -- Province name. Max 25 characters.
-    province    VARCHAR(25),
+
+    province    VARCHAR(25)
 );
 
-CREATE TABLE cities (
-    -- City ID and respective province ID.
+CREATE TABLE IF NOT EXISTS cities (
     id          SERIAl PRIMARY KEY,
     prov_id     INT REFERENCES provinces ON DELETE CASCADE,
-    -- City name. Max 25 characters.
-    city        VARCHAR(25),
+
+    city        VARCHAR(25)
 );
 
 
@@ -32,9 +31,9 @@ CREATE TABLE cities (
 ** User roles.
 ** Entrepreneur, Investor (natural or legal person).
 */
-CREATE TABLE roles (
-    -- User type Id and name.
+CREATE TABLE IF NOT EXISTS roles (
     id          SERIAL PRIMARY KEY,
+
     user_role   VARCHAR(15) NOT NULL
 );
 
@@ -42,38 +41,57 @@ CREATE TABLE roles (
 /*
 ** All users table.
 */
-CREATE TABLE users (
-    -- ID. Serial. Primary Key.
+CREATE TABLE IF NOT EXISTS users (
     id          SERIAL PRIMARY KEY,
-    -- Role of user.
     role_id     INT REFERENCES roles ON DELETE RESTRICT,
 
-    -- PERSONAL INFORMATION
-    -- First and last names. Max 25 characters each.
+    -- PERSONAL INFO
     first_name  VARCHAR(25) NOT NULL,
     last_name   VARCHAR(25) NOT NULL,
     -- CUIT/CUIL/DNI something.
     real_id     VARCHAR(15) NOT NULL,
-    -- Country, province, city.
+    -- Location.
     country_id  INT REFERENCES countries ON DELETE RESTRICT,
     prov_id     INT REFERENCES provinces ON DELETE RESTRICT,
     city_id     INT REFERENCES cities ON DELETE RESTRICT,
     -- Aux date. Format should be dd/mm/yyy. For natural persons its birth date.
     aux_date    DATE NOT NULL,
 
-    -- CONTACT INFORMATION
-    -- Email. Max 25 characters.
+    -- CONTACT INFO
     email       VARCHAR(25) NOT NULL,
-    -- Phone number. Optional. Max length is 25. Allows all international phones.
     phone       VARCHAR(25),
-    -- LinkedIn Profile/Chat Link. Optional. Max 100 characters.
     linkedin    VARCHAR(100),
 
-    -- EXTRA INFORMATION
+    -- EXTRA INFO
     -- Profile picture URN. Optional. Max 100 characters.
     profile_pic VARCHAR(100),
+    join_date   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    -- BACK OFFICE INFO
     -- Trust Index. -100 to 100 range. Default 0.
     trust_index SMALLINT NOT NULL DEFAULT 0,
-    -- Registration date.
-    join_date   DATE NOT NULL
+);
+
+/*
+** All projects created.
+*/
+CREATE TABLE IF NOT EXISTS projects (
+    id          SERIAL PRIMARY KEY,
+    owner_id    INT REFERENCES users ON DELETE CASCADE,
+
+    -- TOP INFO
+    project_name    VARCHAR(25) NOT NULL,
+    summary         VARCHAR(250) NOT NULL,
+    cost            INT NOT NULL DEFAULT 0,
+
+    -- EXTRA INFO
+    publish_date    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    update_date     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    images          BOOLEAN NOT NULL,
+    hits            INT NOT NULL DEFAULT 0,
+
+    -- BACK OFFICE INFO
+    aproved         BOOLEAN NOT NULL DEFAULT false,
+    profit_index    INT NOT NULL DEFAULT 0,
+    risk_index      INT NOT NULL DEFAULT 0,
 );
