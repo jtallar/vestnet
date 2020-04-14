37a18dc5ebc0eda4db6a990b4f77bc66abf30a03
mbrula <mbrula@itba.edu.ar>
2020-04-14 09:31:40 -0300

    Bug fix maven clean package, bad import

--- paw/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -20,7 +20,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.jws.WebParam;
 import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
