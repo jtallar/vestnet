fbae325dc82785f665d36c329a14468bd5345e11
mbrula <mbrula@itba.edu.ar>
2020-05-09 19:49:06 -0300

    code refactor/comment pat 1 - webapp - auth/config

--- paw/webapp/src/main/java/ar/edu/itba/paw/webapp/config/RoleCookieSuccessHandler.java
+++ paw/webapp/src/main/java/ar/edu/itba/paw/webapp/config/RoleCookieSuccessHandler.java
@@ -1,15 +1,11 @@
 package ar.edu.itba.paw.webapp.config;
 
 import ar.edu.itba.paw.webapp.cookie.CookieUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
