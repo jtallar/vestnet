1dd716aa2d953880ff36953e1cccca40e15d17c1
Julian Tallar <jtallar@itba.edu.ar>
2020-12-12 11:57:03 -0300

    Finished JWT token refresh, still needs some testing

--- paw/angular/app/scripts/services/AuthenticatedRestangular.js
+++ paw/angular/app/scripts/services/AuthenticatedRestangular.js
@@ -1,6 +1,6 @@
 define(['paw2020a', 'restangular', 'services/AuthenticationService', 'services/PathService'], function(paw2020a) {
     'use strict';
-    paw2020a.service('AuthenticatedRestangular', ['Restangular', 'AuthenticationService', 'PathService', '$log', '$timeout', function(Restangular, AuthenticationService, PathService, $log, $timeout) {
+    paw2020a.service('AuthenticatedRestangular', ['Restangular', 'AuthenticationService', 'PathService', '$log', '$timeout', '$route', function(Restangular, AuthenticationService, PathService, $log, $timeout, $route) {
       return Restangular.withConfig(function (RestangularConfigurer) {
         // cannot use RestangularConfigurer.setDefaultHeaders(), it is called once and before token set
         // This requests the token on each request, when this instance is made token should already be set
@@ -17,7 +17,7 @@ define(['paw2020a', 'restangular', 'services/AuthenticationService', 'services/P
           if (response.status === 401) {
             AuthenticationService.refresh().then(function () {
               console.log('post refresh');
-              // TODO: Should retry operation or reload this part, but not working
+              // TODO: Probar si esto funca bien
               // Restangular.all().customOperation(response.config.method, response.config.url);
               // $scope.$apply(); // Should not be using scope in service
               // if (!$rootScope.$$phase) $rootScope.$apply();
@@ -25,6 +25,12 @@ define(['paw2020a', 'restangular', 'services/AuthenticationService', 'services/P
               // $timeout(function () {
               //   PathService.get().reload();
               // }, 0);
+              // return $timeout(function() {
+              //   var $http = $injector.get('$http');
+              //   response.config.headers.Authorization = AuthenticationService.getHeaderContent();
+              //   return $http(response.config);
+              // }, 0);
+              $route.reload();
             }, function (errorResponse) {
               console.log('post refresh - error');
               $timeout(function () {
@@ -35,13 +41,16 @@ define(['paw2020a', 'restangular', 'services/AuthenticationService', 'services/P
           } else if (response.status === 403) {
             if (AuthenticationService.isLoggedIn()) {
               console.log('logged in, but unauthorized');
-              // Forbidden entrance, should show 404 or forbidden page
-              return true;
+              PathService.get().forbidden().go();
+              return false;
             } else {
               console.log('redirecting to login');
               PathService.get().login().go();
               return false;
             }
+          } else if (response.status === 404) {
+            PathService.get().notFound().go();
+            return false;
           }
           // 404 should show 404 error page, should be default behaviour
           return true;
