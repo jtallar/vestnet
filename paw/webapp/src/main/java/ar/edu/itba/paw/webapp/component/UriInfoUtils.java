020e0d4ec789294e109a44a1e7ed23563574762f
Matias Brula <mbrula@itba.edu.ar>
2021-02-03 00:17:52 -0300

    fix get base uri for it to work at deploy

--- paw/webapp/src/main/java/ar/edu/itba/paw/webapp/component/UriInfoUtils.java
+++ paw/webapp/src/main/java/ar/edu/itba/paw/webapp/component/UriInfoUtils.java
@@ -11,8 +11,19 @@ public class UriInfoUtils {
     public static URI getBaseURI(UriInfo uriInfo) {
         try {
             final URL baseUrlWithPath = uriInfo.getBaseUri().toURL();
-            final URL finalUrl = new URL(baseUrlWithPath.getProtocol(), baseUrlWithPath.getHost(), baseUrlWithPath.getPort(), "/#");
+
+            /** Strip the path in search of the application context
+             * Could be /api/ or /applicationContext/api/
+             * In case is the second we use it.
+             * */
+            final String[] aux = baseUrlWithPath.getPath().split("\\/");
+            String applicationContext = "";
+            if (aux.length > 2) applicationContext = "/" + aux[1];
+
+            /** Create the final url, adding the /# for Angular */
+            final URL finalUrl = new URL(baseUrlWithPath.getProtocol(), baseUrlWithPath.getHost(), baseUrlWithPath.getPort(), applicationContext + "/#");
             return new URI(finalUrl.toString());
+
         } catch (MalformedURLException | URISyntaxException e) {
             return null;
         }
