5680788bda078593aac7afb856cd9babf400c591
francisco choi <fchoi@itba.edu.ar>
2021-02-02 23:54:46 -0300

    initial set up dependences installed

--- paw/angular/karma.conf.js
+++ paw/angular/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'test-main.js',
-      pattern('')
+      {pattern:'app/tests/feedCtrlTest/*.js', included:true}
     ],
 
 
