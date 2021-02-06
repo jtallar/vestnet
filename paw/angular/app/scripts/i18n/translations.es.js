8a9d323a9d120992ce38e5361f01b974561cb07d
Julian Tallar <jtallar@itba.edu.ar>
2021-02-06 13:34:17 -0300

    Fix translation

--- paw/angular/app/scripts/i18n/translations.es.js
+++ paw/angular/app/scripts/i18n/translations.es.js
@@ -192,7 +192,7 @@ define([], function() {
     accepted: 'Aceptado',
     rejected: 'Rechazado',
     myProjects: 'Mis Proyectos',
-    showAccProj: 'Proyectos no publicados',
+    showAccProj: 'Proyectos pausados',
     showCurrProj: 'Proyectos publicados',
     stopFunding: 'Detener publicación',
     cost: 'Costo',
